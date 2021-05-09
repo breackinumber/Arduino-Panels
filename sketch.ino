63c345ee93a9c5edfa8a3c94d0b78e60f3a7f3c1
Artur <77571639+breackinumber@users.noreply.github.com>
2021-05-09 21:17:36 +0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -1,16 +1,22 @@
-#include <Wire.h>
-#include <DS3231.h>
-RTClib myRTC;
-int expected_position = 0;
-int real_position = 0;
-
-int alltime = 1500;
-
+int oneturn = 90;  //seconds for onehour turn
+int timeout = 1800;  //seconds until breakdown
 #define evening_stopper 0
 #define morning_stopper 0
 #define moto 0
 #define revers 0
 #define power 0
+
+int expected_position = 0;
+int real_position = 0;
+int clocker = 0;
+#include <Wire.h>
+#include <DS3231.h>
+RTClib myRTC;
+
+int getTime() { //method to recieve {int hours} from clocks
+  DateTime current = myRTC.now();
+  return current.hour();
+}
 
 void setup() {
   pinMode(morning_stopper, INPUT);
@@ -19,58 +25,73 @@ void setup() {
   pinMode(revers, OUTPUT);
   pinMode(power, OUTPUT);
   digitalWrite(moto, HIGH);
+  digitalWrite(revers, LOW);
+  digitalWrite(power, LOW);
+
+  while (digitalRead(morning_stopper) != LOW && clocker < (timeout * 10)) {
+    delay(100);   //Go to the morning stopper
+    clocker++;
+  }
   digitalWrite(revers, HIGH);
   digitalWrite(power, HIGH);
-
-
 }
 
 void loop() {
-switch(getTime()){
-  case 10:
-    expected_position = 1;
-    break;
-  case 11:
-    expected_position = 2;
-    break;
-  case 12:
-    expected_position = 3;
-    break;
-  case 13:
-    expected_position = 4;
-    break;
-  case 14:
-    expected_position = 5;
-    break;
-  case 15:
-    expected_position = 6;
-    break;
-   case 16:
-    expected_position = 7;
-    break;
+  clocker = 0;
+  int difference = 0;
+
+  switch (getTime()) {
+    case 10:
+      expected_position = 1;
+      break;
+    case 11:
+      expected_position = 2;
+      break;
+    case 12:
+      expected_position = 3;
+      break;
+    case 13:
+      expected_position = 4;
+      break;
+    case 14:
+      expected_position = 5;
+      break;
+    case 15:
+      expected_position = 6;
+      break;
+    case 16:
+      expected_position = 7;
+      break;
+    default:
+      expected_position = 0;
+      break;
   }
 
-  int difference = expected_position - real_position;
+  difference = expected_position - real_position;
   real_position = expected_position;
 
-  if(difference>0){
-    int steps = alltime * 10 * difference;
-    while (steps>0){
-      if(digitalRead(evening_stopper)!=LOW){
+  if (difference == 0) {
+    delay(10000);
+  } else if (difference > 0) {
+    int steps = oneturn * 10 * difference;
+    while (steps > 0) {
+      if (digitalRead(evening_stopper) != LOW) {
         digitalWrite(power, LOW);
         digitalWrite(moto, LOW);
         delay(100);
         steps--;
       }
-      }
+    }
     digitalWrite(power, HIGH);
     digitalWrite(moto, HIGH);
-    difference = 0;
+
+  } else {
+    while (digitalRead(evening_stopper) != LOW && clocker < timeout * 10) {
+      digitalWrite(power, LOW);
+      digitalWrite(moto, LOW);
+      clocker++;
     }
-
-}
-
-int getTime() {
-  DateTime current = myRTC.now();
-  return current.hour();
+    digitalWrite(power, HIGH);
+    digitalWrite(moto, HIGH);
+  }
 }
