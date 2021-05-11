c8b70ff610d174b8b08184faaba0e1203b845fa5
Artur <77571639+breackinumber@users.noreply.github.com>
2021-05-11 10:40:40 +0300

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -10,15 +10,18 @@ int expected_position = 0;
 int real_position = 0;
 int clocker = 0;
 #include <Wire.h>
-#include <DS3231.h>
-RTClib myRTC;
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 
 int getTime() { //method to recieve {int hours} from clocks
-  DateTime current = myRTC.now();
-  return current.hour();
+  tmElements_t tm;
+  RTC.read(tm);
+  return tm.Hour;
 }
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Setup started");
   pinMode(morning_stopper, INPUT);
   pinMode(evening_stopper, INPUT);
   pinMode(moto, OUTPUT);
@@ -28,15 +31,22 @@ void setup() {
   digitalWrite(revers, LOW);
   digitalWrite(power, LOW);
 
+  Serial.println("Entering the loop for revers");
   while (digitalRead(morning_stopper) != LOW && clocker < (timeout * 10)) {
     delay(100);   //Go to the morning stopper
     clocker++;
+    Serial.print("Reversing: ");
+    Serial.print(clocker);
+    Serial.print("/");
+    Serial.println(timeout * 10);
   }
   digitalWrite(revers, HIGH);
   digitalWrite(power, HIGH);
+  Serial.println("Setup is done successfully");
 }
 
 void loop() {
+  Serial.println("New loop began");
   clocker = 0;
   int difference = 0;
 
@@ -62,23 +72,43 @@ void loop() {
     case 16:
       expected_position = 7;
       break;
+    case 17:
+      expected_position = 7;
+      break;
+    case 18:
+      expected_position = 7;
+      break;
+    case 19:
+      expected_position = 7;
+      break;
+    case 20:
+      expected_position = 7;
+      break;
     case 21:
       expected_position = 0;
       break;
   }
+  Serial.print("Current time is ");
+  Serial.println(getTime());
 
   difference = expected_position - real_position;
   real_position = expected_position;
 
   if (difference == 0) {
+    Serial.println("So idling");
     delay(10000);
   } else if (difference > 0) {
+    Serial.print("Ongoint to evening ");
+    Serial.print(difference);
+    Serial.println( " step(s)");
     int steps = oneturn * 10 * difference;
     while (steps > 0) {
       if (digitalRead(evening_stopper) != LOW) {
         digitalWrite(power, LOW);
         digitalWrite(moto, LOW);
         delay(100);
+        Serial.print("Progress: ");
+        Serial.println(steps);
         steps--;
       }
     }
@@ -86,12 +116,16 @@ void loop() {
     digitalWrite(moto, HIGH);
 
   } else {
+    Serial.println("Going to the morning");
     while (digitalRead(evening_stopper) != LOW && clocker < timeout * 10) {
       digitalWrite(power, LOW);
       digitalWrite(moto, LOW);
       clocker++;
+      Serial.print("Progress: ");
+      Serial.println(clocker);
     }
     digitalWrite(power, HIGH);
     digitalWrite(moto, HIGH);
   }
+  Serial.println("Cycle has been finished successfully");
 }
